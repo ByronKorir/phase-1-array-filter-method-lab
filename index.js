c58b997f39811b904d305340bf1f30631894c5ce
byron korir <byron.korir@student.moringaschool.com>
2023-09-26 10:10:59 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // Code your solution here
-// function findMatching(drivers, element){
-//    return drivers.filter(funtion driver())
-// }
+function findMatching(drivers, element){
+   return drivers.filter(function (driver) { 
+      return driver.toLowerCase() === element.toLowerCase() })
+
+}
+
+function fuzzyMatch(drivers, element){
+   return drivers.filter(function(driver){
+      return driver.toLowerCase().substring(0,element.length) === element.toLowerCase()
+   })
+}
+
+function matchName(drivers, element){
+   return drivers.filter(function (driver){
+     return driver.name === element
+   })
+}
